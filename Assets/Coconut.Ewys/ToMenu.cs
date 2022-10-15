ab4efe93b92604ef6eb16a0cb357ab53ab968f19
YBYCS <87234506+YBYCS@users.noreply.github.com>
2022-10-15 17:58:23 +0800

    Add loading menu scene function

---
+++ Assets/Coconut.Ewys/ToMenu.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Coconut.Ewys {
+	public class ToMenu : MonoBehaviour
+	{
+		void Update()
+		{
+			//Load menu scene when player press mouse or space
+			if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.Mouse0)) {
+				SceneManager.LoadSceneAsync("Menu");
+			}
+		}
+	}
+}
+
+
