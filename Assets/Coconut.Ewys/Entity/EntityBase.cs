86da2532f09c40198edafbd3223c0d22018e11ab
PopSlime <popslime@qq.com>
2022-10-15 19:16:32 +0800

    Add level IO.

---
+++ Assets/Coconut.Ewys/Entity/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Coconut.Ewys.Entity {
+	public abstract class EntityBase : MonoBehaviour {
+		public abstract void FromData(EntityData data);
+		public abstract EntityData ToData();
+	}
+}
---
+++ Assets/Coconut.Ewys/LevelIO.cs
@@ -0,0 +1,47 @@
+﻿using Coconut.Ewys.Entity;
+using Cryville.Common.Unity;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace Coconut.Ewys {
+	public class LevelIO {
+		readonly string _path;
+		readonly Transform _root;
+		public LevelIO(string path, Transform root) {
+			_path = path;
+			_root = root;
+		}
+		public void Read() {
+			foreach (Transform child in _root) GameObject.Destroy(child.gameObject);
+			var level = JsonConvert.DeserializeObject<LevelData>(Resources.Load<TextAsset>(_path).text);
+			foreach (var tile in level.tiles) {
+				var go = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/Tile"));
+				go.transform.SetParent(_root);
+				go.transform.position = (Vector3Int)tile.ToVector2Int();
+			}
+			foreach (var entity in level.entities) {
+				var go = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/Entities/" + entity.type));
+				go.transform.SetParent(_root);
+				go.GetComponent<EntityBase>().FromData(entity);
+			}
+		}
+		public void Write() {
+#if !UNITY_EDITOR_WIN
+			throw new System.InvalidOperationException("Writing level is only supported in the editor.");
+#else
+			var level = new LevelData();
+			foreach (Transform child in _root) {
+				if (child.TryGetComponent<EntityBase>(out var entity)) {
+					level.entities.Add(entity.ToData());
+				}
+				else {
+					level.tiles.Add(Vector2Int.RoundToInt(child.position).ToArray());
+				}
+			}
+			File.WriteAllText(Application.dataPath + "/Resources/Levels/" + _path, JsonConvert.SerializeObject(level), Encoding.UTF8);
+#endif
+		}
+	}
+}
