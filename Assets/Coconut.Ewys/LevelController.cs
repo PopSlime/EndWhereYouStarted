6f2a512fe312d9ab20ea6713a0543b34c4c1119f
PopSlime <popslime@qq.com>
2022-10-20 23:52:38 +0800

    Code cleanup.

--- Assets/Coconut.Ewys/LevelController.cs
+++ Assets/Coconut.Ewys/LevelController.cs
@@ -88,7 +88,7 @@ namespace Coconut.Ewys {
 					if (_currentOp > _level.steps) {
 						foreach (var entity in _entities.Values) entity.OnPhaseUpdate(Side.Lunar);
 						foreach (var entity in _entities.Values) entity.Position = entity.Position;
-							var atom = new TintLightAtom(new Color(0.9f, 0.9f, 1), 0.6f, 1); atom.Do();
+						var atom = new TintLightAtom(new Color(0.9f, 0.9f, 1), 0.6f, 1); atom.Do();
 						_ops.Insert(_currentOp--, atom);
 						_lunarPhase = true;
 					}
