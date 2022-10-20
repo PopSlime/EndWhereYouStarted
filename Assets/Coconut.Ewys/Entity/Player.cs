66176dfd77c5e3e9212ad7acfac0baa603dc0c1e
PopSlime <popslime@qq.com>
2022-10-20 23:37:35 +0800

    Add camp.

--- Assets/Coconut.Ewys/Entity/Player.cs
+++ Assets/Coconut.Ewys/Entity/Player.cs
@@ -2,12 +2,19 @@
 
 namespace Coconut.Ewys.Entity {
 	public class Player : Weight {
+		[SerializeField]
+		GameObject m_prefabCamp;
+
 		Vector2Int _home;
 		public bool IsHome => Position == _home;
 
 		protected override void FromDataImpl(EntityData data) {
 			base.FromDataImpl(data);
 			_home = Position;
+
+			var camp = GameObject.Instantiate(m_prefabCamp);
+			camp.transform.SetParent(LevelController.Instance.transform);
+			camp.transform.position = (Vector3Int)_home;
 		}
 
 		protected override void OnStartMove(Vector2Int delta, bool teleport) {
